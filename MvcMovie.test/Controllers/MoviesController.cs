e1de7dabf1b67a64aaa505ecbadc154203a8a4c2
Tom Vanswijgenhoven <Tom.Vanswijgenhoven@hotmail.com>
2018-04-26 20:50:10 +0200

    Pre-Validation

--- MvcMovie.test/Controllers/MoviesController.cs
+++ MvcMovie.test/Controllers/MoviesController.cs
@@ -56,7 +56,7 @@ namespace MvcMovie.test.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace MvcMovie.test.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
